70f13bb8b119b5262e87379b241097510c7b9c35
Purple4pur <purple4pur@gmail.com>
2023-04-08 23:27:46 +0800

    update

--- run.sh
+++ run.sh
@@ -14,6 +14,15 @@ help_menu() {
     echo "      $0 -h         # Get this help menu"
 }
 
+set_port() {
+    if [[ "$1" -ge 1024 && "$1" -le 65535 ]]; then
+        port=$1
+    else
+        echo "Invalid port number. (1024-65535)"
+        exit 1
+    fi
+}
+
 check_and_install_jq() {
     if ! command -v jq > /dev/null 2>&1; then
         echo "================================================================"
@@ -58,6 +67,7 @@ write_config_file() {
     echo "                       Write config.json"
     echo "================================================================"
     if [[ -e $config_file ]]; then
+        check_and_install_jq
         uuid=$(jq .inbounds[0].settings.clients[0].id $config_file | sed 's/"//g')
         mv $config_file ${config_file}.bak
         echo "Current config.json is backed up: ${config_file}.bak"
@@ -128,12 +138,7 @@ while [[ $# -ne 0 ]]; do
             ;;
         -p | --port)
             shift
-            if [[ "$1" -ge 1024 && "$1" -le 65535 ]]; then
-                port=$1
-            else
-                echo "Invalid port number. (1024-65535)"
-                exit 1
-            fi
+            set_port "$1"
             ;;
         -v | --status)
             read_from_config
